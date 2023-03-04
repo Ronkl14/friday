9c3f1dc9ece1f1e1ac0d3da259925030342f0c3f
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-04 17:41:53 +0200

    working on prefernces page

--- src/pages/Preferences.js
+++ src/pages/Preferences.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../utils/firebase";
 
 const Preferences = () => {
+  const [userPreferences, setUserPreferences] = useState({
+    hangoutRange: null,
+    hangoutType: [],
+    hangoutWith: [],
+    location: null,
+  });
+
+  const auth = getAuth();
+  const docRef = doc(db, "users", auth.currentUser.uid);
+
+  useEffect(() => {
+    (async () => {
+      const docSnap = await getDoc(docRef);
+      const docData = docSnap.data();
+      setUserPreferences({ ...userPreferences });
+    })();
+  }, []);
+
   return <div>Preferences</div>;
 };
 
