2b1ac5c91f6f65e9a41861e568703ea63c15a934
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-04 23:47:20 +0200

    reformat code

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDYoAv6TY0N9Kq_HGmoqh2Q529setn0ql4",
-  authDomain: "friday-46ccb.firebaseapp.com",
-  projectId: "friday-46ccb",
-  storageBucket: "friday-46ccb.appspot.com",
-  messagingSenderId: "1088072710873",
-  appId: "1:1088072710873:web:4b4f679611244660525cc8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
