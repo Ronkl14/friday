a3022961ce9030052fb36410eac74a31438c2c0b
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-06 00:06:50 +0200

    fixed react warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+// import React, { useState, useEffect } from "react";
+import { getAuth, signOut } from "firebase/auth";
 
 const Navbar = () => {
   //   const [displayName, setDisplayName] = useState("");
