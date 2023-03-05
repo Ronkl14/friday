1a2629422cb4cf2b04a56139c62e497ed6c99026
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-06 00:28:41 +0200

    installed package

--- src/components/AddressInput.js
+++ src/components/AddressInput.js
@@ -1,57 +1,57 @@
-// import {
-//   Combobox,
-//   ComboboxInput,
-//   ComboboxList,
-//   ComboboxOption,
-//   ComboboxPopover,
-// } from "@reach/combobox";
-// import React from "react";
-// import usePlacesAutocomplete, {
-//   getGeocode,
-//   getLatLng,
-// } from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxList,
+  ComboboxOption,
+  ComboboxPopover,
+} from "@reach/combobox";
+import React from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
-// const AddressInput = ({ setAddress }) => {
-//   const {
-//     ready,
-//     value,
-//     setValue,
-//     suggestions: { status, data },
-//     clearSuggestions,
-//   } = usePlacesAutocomplete();
+const AddressInput = ({ setAddress }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
 
-//   function addressInputHandler(e) {
-//     setValue(e.target.value);
-//   }
+  function addressInputHandler(e) {
+    setValue(e.target.value);
+  }
 
-//   async function handleSelect(address) {
-//     setValue(address, false);
-//     setAddress(address);
-//     clearSuggestions();
+  async function handleSelect(address) {
+    setValue(address, false);
+    setAddress(address);
+    clearSuggestions();
 
-//     const results = await getGeocode({ address });
-//     const { lat, lng } = getLatLng(results[0]);
-//     console.log("geocode:", lat, lng);
-//   }
+    const results = await getGeocode({ address });
+    const { lat, lng } = getLatLng(results[0]);
+    console.log("geocode:", lat, lng);
+  }
 
-//   return (
-//     <Combobox onSelect={handleSelect}>
-//       <ComboboxInput
-//         value={value}
-//         onChange={addressInputHandler}
-//         disabled={!ready}
-//         placeholder="enter your location"
-//       />
-//       <ComboboxPopover>
-//         <ComboboxList style={{ backgroundColor: "white" }}>
-//           {status === "OK" &&
-//             data.map(({ place_id, description }) => (
-//               <ComboboxOption key={place_id} value={description} />
-//             ))}
-//         </ComboboxList>
-//       </ComboboxPopover>
-//     </Combobox>
-//   );
-// };
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={addressInputHandler}
+        disabled={!ready}
+        placeholder="enter your location"
+      />
+      <ComboboxPopover>
+        <ComboboxList style={{ backgroundColor: "white" }}>
+          {status === "OK" &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
 
-// export default AddressInput;
+export default AddressInput;
