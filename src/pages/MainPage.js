b5172182589528af227cda0bb657bb771869774c
Ronkl14 <121381849+Ronkl14@users.noreply.github.com>
2023-03-06 19:42:17 +0200

    added distance calculation

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,56 @@
 import React from "react";
+import calcDistance from "../utils/distanceCalc";
+import { useEffect, useState } from "react";
+import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+import { db } from "../utils/firebase";
+import { getAuth } from "firebase/auth";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const [places, setPlaces] = useState([]);
+  const [userLocation, setUserLocation] = useState({});
+  const auth = getAuth();
+
+  useEffect(() => {
+    const docRef = doc(db, "users", auth.currentUser.uid);
+    const getUser = async () => {
+      const docSnap = await getDoc(docRef);
+      const docData = docSnap.data();
+      setUserLocation(docData.geoPoint);
+    };
+    getUser();
+  }, [auth.currentUser.uid]);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const querySnapshot = await getDocs(collection(db, "places"));
+      const newPlaces = [];
+      querySnapshot.forEach((doc) => {
+        console.log(userLocation);
+
+        newPlaces.push([
+          doc.id,
+          doc.data(),
+          calcDistance(
+            userLocation.latitude,
+            userLocation.longitude,
+            doc.data().geoPoint.latitude,
+            doc.data().geoPoint.longitude
+          ),
+        ]);
+      });
+      setPlaces(newPlaces);
+      console.log(newPlaces);
+    };
+    fetchPlaces();
+  }, [userLocation]);
+
+  return (
+    <div>
+      {places.map((place) => (
+        <p key={place[0]}>{place[1].name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default MainPage;
